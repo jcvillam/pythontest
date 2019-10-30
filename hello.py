9c0bbee12905b340e381518b16b9b39bffe297d5
Juan Villa <jvilla@opentext.com>
2019-10-30 17:40:38 -0400

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,14 @@
+import sys
+import os
+import math
+
+print(sys.version)
+print(sys.executable)
+
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+
+print(greet("World"))
+print(greet("Juan"))
